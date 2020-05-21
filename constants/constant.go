03363ca42a40b76c0a64fb0686b1402105d5d98b
bipurna <bipurna@gmail.com>
2020-05-21 09:29:43 +0545

    const

--- constants/constant.go
+++ constants/constant.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	// write your code here
+	const myConst int = 43
+	//const fConst float64 = math.Sin(1.57) // constant are not allowed to put value that requires in runtime to set in
+	//constant cannot reassain with another valure
+	fmt.Printf("%v, %T\n", myConst, myConst)
 
 }
