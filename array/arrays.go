3a753696c22e9629419c77370282b49168f65296
bipurna <bipurna@gmail.com>
2020-05-22 23:26:14 +0545

    array final submit

--- array/arrays.go
+++ array/arrays.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+//#Arrays
+//##Collection of items with same type
+//## Fixed Size
+//## Declaration styles
+//### a := [3]int{1,2,3}
+//### a := [...]int{1,2,3}
+//### var a [3] int
+
+//## Access via zero vased index
+//### a := [3]int {1,3,5} //a[1]==3
+
+//## len function returns size of array
+//## Copies refer to different underlying data
+
+//# Slices
+//##Backed by array
+//## Creation styles
+//### slice existing array or slice
+//### literal style
+//### via make function
+//#### a := make([]int, 10) // create slice with capacity and length ==10
+//#### a := make([]int, 100) // create slice with capacity==100 and length ==10
+
+//## len function returns size of array
+//## cap function returns length of underlying array
+//## append function to add elements to slice
+//###May cause expensive copy operation if underlying array is too small
+
+//## Copies refer to same underlying array
+
 func main() {
 	//grades := [3]int{97, 85, 93}
 	//or
@@ -43,9 +73,28 @@ func main() {
 
 	// fmt.Println(b)
 
-	a := make([]int, 3, 100)
-	fmt.Println(a)
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
+	//a := make([]int, 3, 100)
+	// a := []int{}
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
+	// a = append(a, 1)
+
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
+	// a = append(a, 2, 3, 4, 5, 6)
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
+
+	a := []int{1, 2, 3, 4, 5}
+	//b := a[:len(a)-1]
+	//fmt.Println(b)
+
+	//slicing middle
+	b := append(a[:2], a[3:]...)
+
+	fmt.Println(b)
 
 }
